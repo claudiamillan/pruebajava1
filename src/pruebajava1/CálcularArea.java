889d0c8ce7a54c8492afc430e679a7975fdcfe7c
claudiamillan <millanc569@gmail.com>
2017-10-30 21:30:38 -0300

    Realizando esta prueba  calcular area java 1 parte 2

---
+++ src/pruebajava1/CálcularArea.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pruebajava1;
+/************************************************************************
+ * Claudia Millan Lopez
+ * @author lab12
+ * Fecha: Octubre 30 de 2017
+ * Programa: Prueba Java 1
+ * Responsabilidad: Calcular el area de un rectangulo
+ * *********************************************************************/ 
+ 
+public class CálcularArea {
+        
+    int areaRectangular;
+    int baseRectangular;
+    int alturaRectangular;
+    
+      
+    
+ public static void main(String[] args)
+ 
+{
+   //TODO CODIGO DEBE APLICARSE AQUI
+    
+    System.out.println("Ingrese el valor de base");
+    System.out.println("Ingrese el valor de altura");
+    System.out.println("Calcular el area");
+    
+    
+   System.out.println("Calcular= base*altura");
+          
+       
+}
+ 
+   
+   
+}
